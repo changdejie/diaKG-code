a842134b93f2bfe6fd22101f98fa1b2ab0aefeef
devin <changdejie@miao.cn>
2021-06-02 16:27:53 +0800

    添加NER

---
+++ NER/evaluate.py
@@ -0,0 +1,44 @@
+# encoding: utf-8
+
+
+import os
+from pytorch_lightning import Trainer
+
+from trainer import BertLabeling
+
+
+def evaluate(ckpt, hparams_file):
+    """main"""
+
+    trainer = Trainer(distributed_backend="ddp")
+
+    model = BertLabeling.load_from_checkpoint(
+        checkpoint_path=ckpt,
+        hparams_file=hparams_file,
+        map_location=None,
+        batch_size=1,
+        max_length=128,
+        workers=0
+    )
+    trainer.test(model=model)
+
+if __name__ == '__main__':
+    # ace04
+    # HPARAMS = "/mnt/mrc/train_logs/ace2004/ace2004_20200911reproduce_epoch15_lr3e-5_drop0.3_norm1.0_bsz32_hard_span_weight0.1_warmup0_maxlen128_newtrunc_debug/lightning_logs/version_0/hparams.yaml"
+    # CHECKPOINTS = "/mnt/mrc/train_logs/ace2004/ace2004_20200911reproduce_epoch15_lr3e-5_drop0.3_norm1.0_bsz32_hard_span_weight0.1_warmup0_maxlen128_newtrunc_debug/epoch=10_v0.ckpt"
+    # DIR = "/mnt/mrc/train_logs/ace2004/ace2004_20200910_lr3e-5_drop0.3_bert0.1_bsz32_hard_loss_bce_weight_span0.05"
+    # CHECKPOINTS = [os.path.join(DIR, x) for x in os.listdir(DIR)]
+
+    # ace04-large
+    # HPARAMS = "/mnt/mrc/train_logs/ace2004/ace2004_20200910reproduce_lr3e-5_drop0.3_norm1.0_bsz32_hard_span_weight0.1_warmup0_maxlen128_newtrunc_debug/lightning_logs/version_2/hparams.yaml"
+    # CHECKPOINTS = "/mnt/mrc/train_logs/ace2004/ace2004_20200910reproduce_lr3e-5_drop0.3_norm1.0_bsz32_hard_span_weight0.1_warmup0_maxlen128_newtrunc_debug/epoch=10.ckpt"
+
+    # ace05
+    # HPARAMS = "/mnt/mrc/train_logs/ace2005/ace2005_20200911_lr3e-5_drop0.3_norm1.0_bsz32_hard_span_weight0.1_warmup0_maxlen128_newtrunc_debug/lightning_logs/version_0/hparams.yaml"
+    # CHECKPOINTS = "/mnt/mrc/train_logs/ace2005/ace2005_20200911_lr3e-5_drop0.3_norm1.0_bsz32_hard_span_weight0.1_warmup0_maxlen128_newtrunc_debug/epoch=15.ckpt"
+
+    # zh_msra
+    CHECKPOINTS = "E:\\data\\modelNER\\version_7\\checkpoints\\epoch=2.ckpt"
+    HPARAMS = "E:\\data\\modelNER\\version_7\\hparams.yaml"
+
+    evaluate(ckpt=CHECKPOINTS, hparams_file=HPARAMS)
---
+++ NER/utils/get_parser.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+
+import argparse
+
+
+def get_parser() -> argparse.ArgumentParser:
+    """
+    return basic arg parser
+    """
+    parser = argparse.ArgumentParser(description="Training")
+
+    parser.add_argument("--data_dir", type=str, required=False, default="E:\\data\\nested",help="data dir")
+    parser.add_argument("--bert_config_dir", type=str, required=False,  default="E:\data\chinese_roberta_wwm_large_ext_pytorch",help="bert config dir")
+    parser.add_argument("--pretrained_checkpoint", default="", type=str, help="pretrained checkpoint path")
+    parser.add_argument("--max_length", type=int, default=128, help="max length of dataset")
+    parser.add_argument("--batch_size", type=int, default=2, help="batch size")
+    parser.add_argument("--lr", type=float, default=2e-5, help="learning rate")
+    parser.add_argument("--workers", type=int, default=0, help="num workers for dataloader")
+    parser.add_argument("--weight_decay", default=0.01, type=float,
+                        help="Weight decay if we apply some.")
+    parser.add_argument("--warmup_steps", default=0, type=int,
+                        help="warmup steps used for scheduler.")
+    parser.add_argument("--adam_epsilon", default=1e-8, type=float,
+                        help="Epsilon for Adam optimizer.")
+
+    return parser
